71b431bdb4c0c962adeca617ae8756760bca51ec
Taras Pushkar <pushkint@gmail.com>
2024-05-14 16:44:17 +0300

    Initial Commit

---
+++ RickAndMortyTCA/RickAndMortyTCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyTCAApp.swift
+//  RickAndMortyTCA
+//
+//  Created by Taras Pushkar on 14.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyTCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
